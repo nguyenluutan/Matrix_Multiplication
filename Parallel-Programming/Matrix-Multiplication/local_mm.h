3d3eebb7b15ed66019ea65853499513045024adb
Eric <ejahnke13@t-online.de>
2021-11-28 10:40:14 +0100

    Just created new folders and distributed the files from my "Parallel-Programming"

--- Parallel-Programming/Matrix-Multiplication/local_mm.h
+++
@@ -1,4 +0,0 @@
-
-void local_mm(const int m, const int n, const int k, const double alpha,
-    const double *A, const int lda, const double *B, const int ldb,
-    const double beta, double *C, const int ldc);
