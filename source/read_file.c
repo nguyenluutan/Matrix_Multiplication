858515164b9ee3b2447398d5013e67564b23ed7c
Nguyen Luu <nguyen.luutan@studenti.unitn.it>
2021-12-07 16:43:28 +0700

    Regenerate dataset that could avoid using regex

--- source/read_file.c
+++ source/read_file.c
@@ -1,41 +1,91 @@
+// https://stackoverflow.com/questions/3501338/c-read-file-line-by-line
+// https://www.geeksforgeeks.org/regular-expressions-in-c/
+
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <regex.h>
 
-int main() {
-  // FILE *fp; // declaration of file pointer
-  // char con[100000]; // variable to read the content
+int print_result(int value);
+int try_regex(char* first_line);
 
-  // fp = fopen("./Data/data_12_07_2021_08_18/Test_1_8x4_True_10_0.txt", "r");
-  // if (!fp)
-  //   printf("That is not possible with this file!");
-  
-  // printf("Reading file");
+int main()
+{
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char *file_name = "../Data/data_small/Test_151_3x7_dot_7x8_False_10000_0.txt";
 
-  // while(fgets(con, 100000, fp) != NULL)
-  //   printf("%s", con);
+	fp = fopen(file_name, "r");
+	if (fp == NULL)
+		exit(EXIT_FAILURE);
 
-  // fclose(fp);
+	bool first_line = true;
+	while ((read = getline(&line, &len, fp)) != -1)
+	{
+		if (first_line)
+		{
+			printf("Process first line here\n");
+			try_regex(line);
+			first_line = false;
+			continue;
+		}
 
-    FILE * fp;
-    char * line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    char * file_name = "../Data/data_small/Test_4_6x1_True_10_0.txt";
+		printf("%s", line);
+	}
 
-    fp = fopen("../Data/data_small/Test_4_6x1_True_10_0.txt", "r");
-    if (fp == NULL)
-        exit(EXIT_FAILURE);
+	fclose(fp);
+	if (line)
+		free(line);
+	exit(EXIT_SUCCESS);
 
-    while ((read = getline(&line, &len, fp)) != -1) {
-        // printf("Retrieved line of length %zu:\n", read);
-        printf("%s", line);
-    }
+	return 0;
+}
 
-    fclose(fp);
-    if (line)
-        free(line);
-    exit(EXIT_SUCCESS);
+int try_regex(char* first_line)
+{
+	// Variable to create regex
+	regex_t regex;
 
-  return 0;
+	// Variable to store the return
+	// value after creation of regex
+	int value;
+
+	// Function call to create regex
+	value = regcomp(&regex, "[:word:]", 0);
+
+	// If compilation is successful
+	if (value == 0)
+		printf("RegEx compiled successfully.");
+	else
+		printf("Compilation error.");
+
+	value = regexec(&regex, first_line, 0, NULL, 0);
+	print_result(value);
+	return 0;
+}
+
+// Function to print the result
+int print_result(int value)
+{
+	// If pattern found
+	if (value == 0)
+	{
+		printf("Pattern found.\n");
+		return 0;
+	}
+
+	// If pattern not found
+	if (value == REG_NOMATCH)
+	{
+		printf("Pattern not found.\n");
+		return 0;
+	}
+
+	// If error occured during Pattern
+	// matching
+	printf("An error occured.\n");
+	return 0;
 }
